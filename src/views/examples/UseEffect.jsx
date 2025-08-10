ff8e0ec72697074ac9b1a0e809a598cc1dfa6aef
Luciano Silva <legostr@gmail.com>
2025-08-10 14:33:53 -0300

    Add SectionTitle component for exercise headings in UseEffect

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
 function calcFatorial(num) {
   const n = parseInt(num);
@@ -36,6 +37,7 @@ const UseEffect = (props) => {
         title="Hook UseEffect"
         subtitle="Permite executar efeitos colaterais em componentes funcionais!"
       />
+      <SectionTitle title="Exercicio #01" />
 
       <div className="center">
         <div>
@@ -51,6 +53,8 @@ const UseEffect = (props) => {
           onChange={(e) => setNumber(e.target.value)}
         />
       </div>
+      <SectionTitle title="Exercicio #02" />
+      <div className="center">teste</div>
     </div>
   );
 };
