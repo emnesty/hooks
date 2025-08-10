16827e6994778aaf1ce87e0c78bf146425689b86
Luciano Silva <legostr@gmail.com>
2025-08-10 14:08:52 -0300

    Add useState hook to manage number input in UseEffect component

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useState } from "react";
+import PageTitle from "../../components/layout/PageTitle";
 
 const UseEffect = (props) => {
-    return (
-        <div className="UseEffect">
-            <PageTitle
-                title="Hook UseEffect"
-                subtitle="Permite executar efeitos colaterais em componentes funcionais!"
-            />
-        </div>
-    )
-}
+  const [number, setNumber] = useState(1);
 
-export default UseEffect
+  return (
+    <div className="UseEffect">
+      <PageTitle
+        title="Hook UseEffect"
+        subtitle="Permite executar efeitos colaterais em componentes funcionais!"
+      />
+
+      <div className="center">
+        <div>
+          <span className="text">Fatorial:</span>
+          <span className="text red">100</span>
+        </div>
+        <input
+          type="number"
+          className="input"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default UseEffect;
